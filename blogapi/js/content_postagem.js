01772eb6f65a654867dc99b3d8e54f0f8c2c128e
DiegoJefferson741 <110000429+DiegoJefferson741@users.noreply.github.com>
2022-10-25 15:31:55 -0300

    Add files via upload

--- blogapi/js/content_postagem.js
+++ blogapi/js/content_postagem.js
@@ -30,6 +30,49 @@ async function getPost(pid){
     postContainer.appendChild(text);
 
     //percorrer os comentarios
+    dadosComent.map((comment)=>{
+       insertComment(comment);
+    });
+}
+
+function insertComment(comment){
+    let div = document.createElement("div");
+    let email = document.createElement("h3");
+    let text = document.createElement("p");
+
+    email.innerText = comment.email;
+    text.innerText =comment.body;
+    div.appendChild(email);
+    div.appendChild(text);
+    comentContainer.appendChild(div);
 }
 
 getPost(id);
+
+async function postComment(comment){
+    let response = await fetch(url, {
+        method: "POST",
+        body: comment,
+        headers: {"content-type":"application/json"}
+    });
+    const data = await response.json();
+
+    //console.log(data);
+    insertComment(data);
+
+    document.querySelector("#email").value="";
+    document.querySelector("#coment").value="";
+}
+
+let commentForm = document.querySelector("#coment-form");
+commentForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    let comment = {
+        email: document.querySelector("#email").value,
+        body: document.querySelector("#coment").value
+    };
+    comment = JSON.stringify(comment);
+
+    //Enviar para a API
+    postComment(comment);
+});
